634e05bcbb5395975a8f42a6f12ab068250f2163
tuankiet <tuankiet0303@gmail.com>
2024-05-07 18:50:07 +0700

    update the led depending on the recieved message

--- yolobit/yolobit.py
+++ yolobit/yolobit.py
@@ -1,7 +1,7 @@
 import music
 import time
 from machine        import Pin, SoftI2C
-from aiot_dht20     import DHT20
+# from aiot_dht20     import DHT20
 from yolobit        import *
 from mqtt           import *
 from event_manager  import *
@@ -12,9 +12,26 @@ def on_receive_message_bcc_led(button):
     else:
         display.set_all('#000000')
 
-def update_temperature():
-    aiot_dht20.read_dht20()
-    mqtt.publish('bbc-temp', (aiot_dht20.dht20_temperature()))
+def on_receive_livingroom_led (message:str):
+    message = message.strip().split()
+    if message[2] == "ON":
+        display.set_pixel(0, int(message[1]), '#ff0000')
+    elif message[2] == "OFF":
+        display.set_pixel(0, int(message[1]), '#000000')
+
+def on_receive_kitchen_led (message:str):
+    message = message.strip().split()
+    if message[2] == "ON":
+        display.set_pixel(1, int(message[1]), '#ff0000')
+    elif message[2] == "OFF":
+        display.set_pixel(1, int(message[1]), '#000000')
+
+def on_receive_bedroom_led (message:str):
+    message = message.strip().split()
+    if message[2] == "ON":
+        display.set_pixel(2, int(message[1]), '#ff0000')
+    elif message[2] == "OFF":
+        display.set_pixel(2, int(message[1]), '#000000')
 
 if __name__ =="main":
     button_a.on_pressed = None
@@ -25,14 +42,15 @@ if __name__ =="main":
     display.scroll('HI')
     count = 0
 
-    aiot_dht20 = DHT20(SoftI2C(scl=Pin(22), sda=Pin(21)))
 
     mqtt.connect_wifi ('kiietuan', '0913414256')
-    mqtt.connect_broker(server='io.adafruit.com', port=1883, username='tuankiet0303', password='aio_ATnv88akXoj47WX10OVkeI9iq5T6')
-    mqtt.on_receive_message('bbc-led', on_receive_message_bcc_led)
+    mqtt.connect_broker(server='io.adafruit.com', port=1883, username='tuankiet0303', password='aio_faJu09BavEJu1an6bNeInk5tW648')
+    # mqtt.on_receive_message('bbc-led', on_receive_message_bcc_led)
+    mqtt.on_receive_message('led-slash-livingroom',on_receive_livingroom_led)
+    mqtt.on_receive_message('led-slash-bedroom',on_receive_bedroom_led)
+    mqtt.on_receive_message('led-slash-kitchen',on_receive_kitchen_led)
 
     event_manager.reset()
-    event_manager.add_timer_event(30000, update_temperature)
 
     while True:
         mqtt.check_message()
