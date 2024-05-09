616cf280487eb9c149d5faae9af8d2f609bef8a8
Qmi <minh.pham1126@gmail.com>
2024-05-10 03:36:31 +0700

    feat: make lighting dynamic in backend

--- app/app.py
+++ app/app.py
@@ -23,15 +23,20 @@ CORS(
         r"/light/*": {"origins": "*"},
         r"/light_state/*": {"origins": "*"},
         r"/audio/*": {"origins": "*"},
+        r"/light_sys/*": {"origins": "*"},
     },
 )
 
-camera_streams = [
-    0,
-    "http://192.168.1.36:8080/video"
-]
 
 socketio = SocketIO(app, cors_allowed_origins="*")
+db = {
+    "camera_streams": [0, "http://192.168.1.36:8080/video"],
+    "light_sys": {
+        "bedroom": ["light1", "light2", "light3", "light4", "light5"],
+        "kitchen": ["light1", "light2", "light3", "light4", "light5"],
+        "livingroom": ["light1", "light2", "light3", "light4", "light5"],
+    },
+}
 
 
 @socketio.on("connect")
@@ -65,31 +70,43 @@ async def receive_audio():
         if "error" in response:
             print(f"Error: {response['error']}, Details: {response['details']}")
             return response, 500
-        print(response)
         return response, 200
 
 
-@app.route("/light", methods=["POST"])
+@app.route("/light/sys", methods=["GET"])
+def get_light_system():
+    return db["light_sys"], 200
+
+
+@app.route("/light/control", methods=["POST"])
 def light_switch():
     room = request.json.get("room")
     light_id = request.json.get("light_id")
     state = request.json.get("state")
+    print(request.json)
+    valid_rooms = db["light_sys"].keys()
+    valid_states = ["ON", "OFF"]
 
-    if state not in ["ON", "OFF"]:
+    if state not in valid_states:
         return {"error": "Invalid state"}, 400
 
-    if light_id == "1" or light_id == "2" or light_id == "3" or light_id == "4" or light_id == "5":
-        if room == "bedroom":
-            client.publish(topic=topic_head+"led-slash-bedroom", payload=room + " " + light_id + " " + state)
-        elif room == "livingroom":
-            client.publish(topic=topic_head+"led-slash-livingroom", payload=room + " " + light_id + " " + state)
-        elif room == "kitchen":
-            client.publish(topic=topic_head+"led-slash-kitchen", payload=room + " " + light_id + " " + state)
+    if room not in valid_rooms:
+        return {"error": "Invalid room"}, 400
+
+    if light_id not in db["light_sys"][room]:
+        return {"error": "Invalid light_id for the given room"}, 400
+
+    light_index = db["light_sys"][room].index(light_id)
+
+    client.publish(
+        topic=topic_head + "led-slash-" + room,
+        payload=f"{room} {light_index} {state}",
+    )
 
     return {"room": room, "light_id": light_id, "state": state}, 200
 
 
-@app.route("/light_state", methods=["GET"])
+@app.route("/light/state", methods=["GET"])
 def get_light_state():
     room = request.args.get("room")
     light_id = request.args.get("light_id")
@@ -101,10 +118,10 @@ def get_light_state():
 
 @app.route("/video_feed/<int:id>")
 def video_feed(id):
-    if id < 0 or id >= len(camera_streams):
+    if id < 0 or id >= len(db["camera_streams"]):
         return "Invalid camera ID", 400
 
-    stream_link = camera_streams[id]
+    stream_link = db["camera_streams"][id]
 
     return Response(
         generate_frames(stream_link),
@@ -120,5 +137,4 @@ def index():
 if __name__ == "__main__":
     client = connect_mqtt()
     client.loop_start()
-    # client.publish(topic=topic_head+"led-slash-bedroom", payload="livingroom" + " " + "1" + " " + "ON")
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=os.environ.get("FLASK_PORT"))
