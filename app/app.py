efe29b7a63345702ebd9adc06f7395c9c40c2d08
Qmi <minh.pham1126@gmail.com>
2024-05-18 04:58:28 +0700

    feat(backend): completely convert app from flask to fastapi
    BREAKING CHANGE:

--- app/app.py
+++ app/app.py
@@ -1,62 +1,187 @@
 import asyncio
+import json
+import logging
 import os
-import subprocess
-import time
+from collections import defaultdict
+from contextlib import asynccontextmanager
 from datetime import datetime
+from typing import Dict, List, Optional
 
+import aiomqtt
 import cv2
-from camera import generate_frames, model, threshold
-from database.scripts.light import fetch_light_state, update_light_state
+from camera import generate_frames
+from database.scripts import light
 from database.scripts.sensor import insert_sensor_data
 from dotenv import load_dotenv
-from flask import Flask, Response, request
-from flask_cors import CORS
-from flask_socketio import SocketIO, emit
+from fastapi import (
+    BackgroundTasks,
+    FastAPI,
+    File,
+    HTTPException,
+    UploadFile,
+    WebSocket,
+    WebSocketDisconnect,
+    status,
+)
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse, StreamingResponse
 from local_inference import query
+from motor.motor_asyncio import AsyncIOMotorClient
 from myMqtt import *
-from pymongo import MongoClient
+from pydantic import BaseModel
+from ultralytics import YOLO
 
 load_dotenv()
-
-app = Flask(__name__)
-CORS(
-    app,
-    resources={
-        r"/socket.io/*": {"origins": "*"},
-        r"/light/*": {"origins": "*"},
-        r"/light_state/*": {"origins": "*"},
-        r"/audio/*": {"origins": "*"},
-        r"/light_sys/*": {"origins": "*"},
-    },
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),  # Log to a file
+        logging.StreamHandler(),  # Also log to the console
+    ],
 )
+
+# Get a logger instance
+logger = logging.getLogger(__name__)
+
+# Create an asyncio Queue for message passing
+message_queue = asyncio.Queue()
+
 connection_string = f"mongodb://{os.environ.get('MONGO_USER')}:{os.environ.get('MONGO_PASSWORD')}@localhost:27017/"
-socketio = SocketIO(app, cors_allowed_origins="*")
-db = {
-    "camera_streams": [0, "http://192.168.1.36:8080/video"],
-    "light_sys": {
-        "bedroom": ["light1", "light2", "light3", "light4", "light5"],
-        "kitchen": ["light1", "light2", "light3", "light4", "light5"],
-        "livingroom": ["light1", "light2", "light3", "light4", "light5"],
-    },
-}
+motor_client = AsyncIOMotorClient(connection_string)
+db = motor_client["iot_232"]
+
+# Define collections
+sensor_collection = db["sensor_data"]
+light_collection = db["light"]
+camera_collection = db["camera"]
+os.makedirs("uploads", exist_ok=True)
+
+weight_path = "ai/model.pt"
+
+mqtt_client = None
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(json.dumps({"message": message}))
+
+
+class TaskManager:
+    def __init__(self):
+        self.tasks: Dict[int, asyncio.Task] = {}
+
+    def add_task(self, stream_id: int, task: asyncio.Task):
+        self.tasks[stream_id] = task
+
+    def get_task(self, stream_id: int) -> Optional[asyncio.Task]:
+        return self.tasks.get(stream_id)
+
+    def remove_task(self, stream_id: int):
+        if stream_id in self.tasks:
+            del self.tasks[stream_id]
+
+
+task_manager = TaskManager()
+
+manager = ConnectionManager()
+
+
+async def listen(client):
+    async for message in client.messages:
+
+        if message.topic.matches(getTopicName("led-slash-bedroom")):
+            print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+        if message.topic == getTopicName("led-slash-livingroom"):
+            print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+        if message.topic.matches(getTopicName("led-slash-kitchen")):
+            print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+        if message.topic.matches(getTopicName("sensor-temperature")):
+            print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+            try:
+                await insert_sensor_data(
+                    sensor_id="temp1",
+                    sensor_type="temp",
+                    value=int(message.payload.decode()),
+                    timestamp=datetime.now(),
+                )
+            except Exception as e:
+                print(f"Error inserting sensor data: {e}")
+        if message.topic.matches(getTopicName("sensor-moisture")):
+            print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+            try:
+                await insert_sensor_data(
+                    sensor_id="moist1",
+                    sensor_type="moist",
+                    value=int(message.payload.decode()),
+                    timestamp=datetime.now(),
+                )
+            except Exception as e:
+                print(f"Error inserting sensor data: {e}")
+
+
+@asynccontextmanager
+async def lifespan(app):
+    global mqtt_client
+    async with aiomqtt.Client(
+        identifier=f"python-mqtt-{random.randint(0, 1000)}",
+        hostname=host,
+        port=port,
+        username=username,
+        password=password,
+    ) as c:
+        mqtt_client = c
+        await mqtt_client.subscribe(f"{topic_head}#")
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(listen(mqtt_client))
+        yield
+        # Cancel the task
+        task.cancel()
+        # Wait for the task to be cancelled
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+
+
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 
 def debounce(wait):
-    # Decorator that will postpone a function's
-    # execution until after `wait` seconds
-    # have elapsed since the last time it was invoked.
     def decorator(fn):
-        last_invoked = {}
+        last_invoked = defaultdict(lambda: 0)
 
-        def debounced(stream_link, *args, **kwargs):
+        async def debounced(stream_link, *args, **kwargs):
             nonlocal last_invoked
             now = time.time()
-            if (
-                stream_link not in last_invoked
-                or now - last_invoked[stream_link] >= wait
-            ):
+            if now - last_invoked[stream_link] >= wait:
                 last_invoked[stream_link] = now
-                return fn(stream_link, *args, **kwargs)
+                await fn(stream_link, *args, **kwargs)
 
         return debounced
 
@@ -64,189 +189,190 @@ def debounce(wait):
 
 
 @debounce(1.0)
-def send_notification(message):
-    socketio.emit("notification", {"message": message})
+async def send_notification(stream_link, message):
+    await manager.send_message(message)
+
+
+@app.post("/detection/start")
+async def start_detection(stream_id: int, background_tasks: BackgroundTasks):
+    stream_link = await get_stream_link_from_db(stream_id)
+    if stream_link is None:
+        raise HTTPException(status_code=404, detail="Stream link not found")
+
+    task = asyncio.create_task(only_detect_box(stream_link))
+    task_manager.add_task(stream_id, task)
+    return {"message": "Detection started"}
+
+
+@app.post("/detection/stop")
+async def stop_detection(stream_id: int):
+    logger.info(f"HEREEEE! {stream_id}")
+    task = task_manager.get_task(stream_id)
+    if task is None:
+        raise HTTPException(
+            status_code=404, detail="No detection task found for this stream_id"
+        )
+
+    task.cancel()
+    task_manager.remove_task(stream_id)
+
+    try:
+        await task
+    except asyncio.CancelledError:
+        logger.info(f"Detection task for stream_id {stream_id} has been cancelled")
+
+    return {"message": "Detection stopped"}
 
 
 async def only_detect_box(stream_link):
     stream = cv2.VideoCapture(stream_link)
-    while True:
-        success, frame = stream.read()
-        if not success:
-            break
-        else:
-            b_boxes = model(frame, verbose=False)[0].boxes.data.tolist()
-            count = 0
-            for b_box in b_boxes:
-                x1, y1, x2, y2, score, class_id = b_box
-                if score > threshold:
-                    count += 1
-            if count > 1:
-                message = (
-                    f"{stream_link} currently has {count} numbers of people in the view"
-                )
-                send_notification(stream_link, message)
-        await asyncio.sleep(0)  # Yield control to the event loop
+    threshold = 0.5
+    model = YOLO(weight_path)
+    try:
+        while True:
+            success, frame = stream.read()
+            if not success:
+                break
+            else:
+                b_boxes = model(frame, verbose=False)[0].boxes.data.tolist()
+                count = 0
+                for b_box in b_boxes:
+                    x1, y1, x2, y2, score, class_id = b_box
+                    if score >= threshold:
+                        count += 1
+                if count >= 1:
+                    message = f"{stream_link} currently has {count} numbers of people in the view"
+                    await send_notification(stream_link, message)
+            await asyncio.sleep(0.1)  # Adjust as needed to control frame rate
+    except asyncio.CancelledError:
+        logger.info(f"Detection task for stream_link {stream_link} has been cancelled")
+        stream.release()
+        raise
+    finally:
+        stream.release()
 
 
-@app.before_first_request
-def start_detection_task():
-    for stream_link in db["camera_streams"]:
-        asyncio.create_task(only_detect_box(stream_link))
+async def get_stream_link_from_db(id: int):
+    stream_info = await camera_collection.find_one({"id": id})
+    if stream_info:
+        return stream_info.get("stream_link")
+    else:
+        return None
 
 
-@socketio.on("connect")
-def test_connect():
-    emit("notification", {"message": "New notification!"})
-    for i in range(3):
-        test_message = "New notification! " + str(i)
-        emit("notification", {"message": test_message})
-
-
-@socketio.on("disconnect")
-def test_disconnect():
-    print("Client disconnected")
-
-
-@app.route("/audio", methods=["POST"])
-async def receive_audio():
-    if "file" not in request.files:
-        return "No file part in the request.", 400
-
-    file = request.files["file"]
-
-    if file.filename == "":
-        return "No selected file.", 400
-
-    if file:
+@app.post("/audio")
+async def receive_audio(file: UploadFile = File(...)):
+    try:
+        # Save the uploaded file
         filepath = os.path.join("uploads", file.filename)
-        file.save(filepath)
+        with open(filepath, "wb") as f:
+            f.write(file.file.read())
+
+        # Process the uploaded audio file
         response = await query(filepath)
+
+        # Check for processing errors
         if "error" in response:
-            print(f"Error: {response}, Details: {response['details']}")
-            return response, 500
-        return response, 200
+            error_message = response["error"]
+            raise HTTPException(status_code=500, detail=error_message)
+
+        return JSONResponse(content=response, status_code=200)
+    except HTTPException as http_error:
+        raise http_error
+    except Exception as e:
+        error_message = f"Error processing audio: {str(e)}"
+        raise HTTPException(status_code=500, detail=error_message)
 
 
-@app.route("/light/sys", methods=["GET"])
-def get_light_system():
-    mongo_client = MongoClient(connection_string)
-    iot_db = mongo_client.iot_232
-    light_collection = iot_db["light"]
+@app.get("/video_feed/{id}")
+async def video_feed(id: int):
+    stream_link = await get_stream_link_from_db(id)
+    if stream_link is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid camera ID"
+        )
 
-    result = {}
-    all_data = light_collection.find({})
+    async def frames():
+        async for frame in generate_frames(stream_link):
+            yield frame
 
-    for data in all_data:
-        for room, lights in data.items():
-            if room != "_id":
-                result[room] = [light["name"] for light in lights]
-
-    return result, 200
+    return StreamingResponse(
+        frames(),
+        media_type="multipart/x-mixed-replace; boundary=frame",
+    )
 
 
-@app.route("/light/control", methods=["POST"])
-def light_switch():
-    mongo_client = MongoClient(connection_string)
-    iot_db = mongo_client.iot_232
-    light_collection = iot_db["light"]
+@app.get("/")
+async def index():
+    return {"message": "Hello"}
 
-    room = request.json.get("room")
-    light_id = request.json.get("light_id")
-    state = request.json.get("state")
-    print(request.json)
 
-    valid_states = ["ON", "OFF"]
+@app.get("/light/sys")
+async def get_light_system():
+    try:
+        result = {}
+        async for data in light_collection.find({}):
+            for room, lights in data.items():
+                if room != "_id":
+                    result[room] = [light["name"] for light in lights]
 
-    if state not in valid_states:
-        return {"error": "Invalid state"}, 400
+        return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error fetching light system data: {str(e)}"
+        )
 
-    room_data = light_collection.find_one({room: {"$exists": True}})
+
+async def get_light_index(room: str, light_id: str):
+    room_data = await light_collection.find_one({room: {"$exists": True}})
     if room_data is None:
-        return {"error": "Invalid room"}, 400
+        raise HTTPException(status_code=400, detail="Invalid room")
 
     light_names = [light["name"] for light in room_data[room]]
     if light_id not in light_names:
-        return {"error": "Invalid light_id for the given room"}, 400
+        raise HTTPException(
+            status_code=400, detail="Invalid light_id for the given room"
+        )
 
-    light_index = light_names.index(light_id)
+    return light_names.index(light_id)
 
-    client.publish(
+
+class LightControlRequest(BaseModel):
+    room: str
+    light_id: str
+    state: str
+
+
+@app.post("/light/control")
+async def light_switch(request_body: LightControlRequest):
+    # Extract parameters from the request body
+    room = request_body.room
+    light_id = request_body.light_id
+    state = request_body.state
+
+    if state not in ["ON", "OFF"]:
+        raise HTTPException(status_code=400, detail="Invalid state")
+
+    # Get the index of the light in the room
+    light_index = await get_light_index(room, light_id)
+
+    # Publish MQTT message
+    await mqtt_client.publish(
         topic=topic_head + "led-slash-" + room,
         payload=f"{room} {light_index} {state}",
     )
-    update_light_state(room, light_id, state)
 
-    return {"room": room, "light_id": light_id, "state": state}, 200
+    await light.update_light_state(room, light_id, state)
+
+    return {"room": room, "light_id": light_id, "state": state}
 
 
-@app.route("/light/state", methods=["GET"])
-def get_light_state():
-    room = request.args.get("room")
-    light_id = request.args.get("light_id")
-
-    state = fetch_light_state(room, light_id)
+@app.get("/light/state")
+async def get_light_state(room: str, light_id: str):
+    # Fetch light state from MongoDB
+    state = await light.fetch_light_state(room, light_id)
     if state is None:
-        return {"error": "wrong room or light id"}, 400
-
-    return {"state": state}, 200
-
-
-@app.route("/video_feed/<int:id>")
-def video_feed(id):
-    if id < 0 or id >= len(db["camera_streams"]):
-        return "Invalid camera ID", 400
-
-    stream_link = db["camera_streams"][id]
-
-    return Response(
-        generate_frames(stream_link),
-        mimetype="multipart/x-mixed-replace; boundary=frame",
-    )
-
-
-@app.route("/")
-def index():
-    return "Hello"
-
-
-def getTopicName(name: str) -> str:
-    return topic_head + name
-
-
-def on_message(client, userdata, message):
-    if message.topic == getTopicName("led-slash-bedroom"):
-        print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
-    if message.topic == getTopicName("led-slash-livingroom"):
-        print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
-    if message.topic == getTopicName("led-slash-kitchen"):
-        print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
-    if message.topic == getTopicName("sensor-temperature"):
-        insert_sensor_data(
-            sensor_id="temp1",
-            type="temp",
-            value=int(message.payload.decode()),
-            timestamp=datetime.now(),
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid room or light ID"
         )
-    if message.topic == getTopicName("sensor-moisture"):
-        insert_sensor_data(
-            sensor_id="moist1",
-            type="moist",
-            value=int(message.payload.decode()),
-            timestamp=datetime.now(),
-        )
-
-
-if __name__ == "__main__":
-    mongo_client = MongoClient(connection_string)
-    iot_db = mongo_client.iot_232
-
-    sensor_collection = iot_db["sensor_data"]
-    led_collection = iot_db["light"]
-
-    client = connect_mqtt()
-    client.subscribe(f"{topic_head}#")
-    client.on_message = on_message
-    client.loop_start()
-    app.run(host="0.0.0.0", port=os.environ.get("FLASK_PORT"))
-    client.loop_stop()
+    return {"state": state}
