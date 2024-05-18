7b157e7fdd2ffe7195a45d9a2ab32354694e4854
Qmi <minh.pham1126@gmail.com>
2024-05-19 03:34:58 +0700

    test(frontend): test graph

--- smarthome/src/Pages/testing.tsx
+++ smarthome/src/Pages/testing.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
+
 import Mic from "../Components/Mic";
-
-
+import RealTimeGraph from "../Components/Sensor/RealTimeGraph";
+import { backendURL } from "../Utils/getENV";
 const Testing = () => {
+    const [sensorIds, setSensorIds] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fetchSensorIds = async () => {
+            try {
+                const response = await fetch(`${backendURL}/sensor/ids`);
+                const data: string[] = await response.json();
+                setSensorIds(data);
+            } catch (error) {
+                console.error("Error fetching sensor IDs:", error);
+            }
+        };
+
+        fetchSensorIds();
+    }, []);
+
     return (
         <>
             <Mic />
+            {sensorIds.map((sensorId) => (
+                <RealTimeGraph key={sensorId} sensorId={sensorId} />
+            ))}
         </>
     );
 };
