58f7bd33905b12b36b8c0641c2e65f9823b5208b
Qmi <minh.pham1126@gmail.com>
2024-05-19 03:28:42 +0700

    fix: fix timeseries data bug

---
+++ app/Utils/convert.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+from bson import ObjectId
+
+
+def convert_objectid(data: Any) -> Any:
+    if isinstance(data, list):
+        return [convert_objectid(item) for item in data]
+    elif isinstance(data, dict):
+        return {key: convert_objectid(value) for key, value in data.items()}
+    elif isinstance(data, ObjectId):
+        return str(data)
+    else:
+        return data
