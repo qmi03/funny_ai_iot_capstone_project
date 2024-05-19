afbedd1cf668ddcbc0475798c3234fe6dacba3fb
Qmi <minh.pham1126@gmail.com>
2024-05-19 19:12:19 +0700

    feat(yolobit): update lighting and sensors

--- yolobit/yolobit.py
+++ yolobit/yolobit.py
@@ -1,7 +1,7 @@
 import music
 import time
 from machine        import Pin, SoftI2C
-# from aiot_dht20     import DHT20
+from aiot_dht20     import DHT20
 from yolobit        import *
 from mqtt           import *
 from event_manager  import *
@@ -14,45 +14,64 @@ def on_receive_message_bcc_led(button):
 
 def on_receive_livingroom_led (message:str):
     message = message.strip().split()
-    if message[2] == "ON":
-        display.set_pixel(0, int(message[1]), '#ff0000')
-    elif message[2] == "OFF":
-        display.set_pixel(0, int(message[1]), '#000000')
+    if message[1] == "ALL":
+        if message[2] == "ON":
+            for i in range(5):
+                display.set_pixel(0, i, '#ff0000')
+        elif message[2] == "OFF":
+            for i in range(5):
+                display.set_pixel(0, i, '#000000')
+    else:
+        if message[2] == "ON":
+            display.set_pixel(0, int(message[1]), '#ff0000')
+        elif message[2] == "OFF":
+            display.set_pixel(0, int(message[1]), '#000000')
 
 def on_receive_kitchen_led (message:str):
     message = message.strip().split()
-    if message[2] == "ON":
-        display.set_pixel(1, int(message[1]), '#ff0000')
-    elif message[2] == "OFF":
-        display.set_pixel(1, int(message[1]), '#000000')
-
+    if message[1] == "ALL":
+        if message[2] == "ON":
+            for i in range(5):
+                display.set_pixel(1, i, '#ff0000')
+        elif message[2] == "OFF":
+            for i in range(5):
+                display.set_pixel(1, i, '#000000')
+    else:
+        if message[2] == "ON":
+            display.set_pixel(1, int(message[1]), '#ff0000')
+        elif message[2] == "OFF":
+            display.set_pixel(1, int(message[1]), '#000000')
+            
 def on_receive_bedroom_led (message:str):
     message = message.strip().split()
-    if message[2] == "ON":
-        display.set_pixel(2, int(message[1]), '#ff0000')
-    elif message[2] == "OFF":
-        display.set_pixel(2, int(message[1]), '#000000')
+    if message[1] == "ALL":
+        if message[2] == "ON":
+            for i in range(5):
+                display.set_pixel(2, i, '#ff0000')
+        elif message[2] == "OFF":
+            for i in range(5):
+                display.set_pixel(2, i, '#000000')
+    else:
+        if message[2] == "ON":
+            display.set_pixel(2, int(message[1]), '#ff0000')
+        elif message[2] == "OFF":
+            display.set_pixel(2, int(message[1]), '#000000')
 
-if __name__ =="main":
-    button_a.on_pressed = None
-    button_b.on_pressed = None
-    button_a.on_pressed_ab = button_b.on_pressed_ab = -1
+def update_temperature_humidity():
+    aiot_dht20.read_dht20()
+    temperature = aiot_dht20.dht20_temperature()
+    if (float(temperature) >= 30):
+        pin0.write_analog(round(translate(70, 0, 100, 0, 1023)))
+    else:
+        pin0.write_analog(round(translate(0, 0, 100, 0, 1023)))
+    mqtt.publish('sensor-temperature', (aiot_dht20.dht20_temperature()))
+    mqtt.publish('sensor-moisture', (aiot_dht20.dht20_humidity()))
+    
+button_a.on_pressed = None
+button_b.on_pressed = None
+button_a.on_pressed_ab = button_b.on_pressed_ab = -1
 
 
-    display.scroll('HI')
-    count = 0
-
-
-    mqtt.connect_wifi ('kiietuan', '0913414256')
-    mqtt.connect_broker(server='io.adafruit.com', port=1883, username='tuankiet0303', password='aio_faJu09BavEJu1an6bNeInk5tW648')
-    # mqtt.on_receive_message('bbc-led', on_receive_message_bcc_led)
-    mqtt.on_receive_message('led-slash-livingroom',on_receive_livingroom_led)
-    mqtt.on_receive_message('led-slash-bedroom',on_receive_bedroom_led)
-    mqtt.on_receive_message('led-slash-kitchen',on_receive_kitchen_led)
-
-    event_manager.reset()
-
-    while True:
-        mqtt.check_message()
-        event_manager.run()
-
+display.scroll('HI')
+count = 0
+aiot_dht20 = DHT20(SoftI2C(scl=Pin(22), sda=Pin(21)))
