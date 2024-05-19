4418a5bb52bde944c0d324c7493013039ea3f726
Qmi <minh.pham1126@gmail.com>
2024-05-19 07:34:19 +0700

    feat(database): get implement get latest sensor data

--- app/database/scripts/sensor.py
+++ app/database/scripts/sensor.py
@@ -26,7 +26,27 @@ async def insert_sensor_data(sensor_id, sensor_type, value, timestamp):
         mongo_client.close()
 
 
+async def get_latest_data(sensor_id):
+    mongo_client = AsyncIOMotorClient(connection_string)
+    try:
+        iot_db = mongo_client.iot_232
+        sensor_collection = iot_db["sensor_data"]
+
+        # We sort by timestamp in descending order and get the first document
+        document = await sensor_collection.find_one(
+            {"metadata.sensor_id": sensor_id}, sort=[("timestamp", -1)]
+        )
+
+        if document is not None:
+            value = document.get("value")
+            return value
+        else:
+            return None
+    finally:
+        mongo_client.close()
+
+
 if __name__ == "__main__":
     # Example usage:
     timestamp = datetime(2024, 5, 15, 12, 0, 0)  # Create a datetime object
-    asyncio.run(insert_sensor_data("sensor123", "temperature", 25.5, timestamp))
+    asyncio.run((get_latest_data("temp1")))
